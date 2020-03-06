c496842ad44fb18052f99ee42ac3a29af460ae63
3rdBench <benjie.work@gmail.com>
2020-03-06 18:10:18 +0800

    Remove arrays that stores list items from app.js to simply codebase and then delete file date.js from project folder.

--- app.js
+++ app.js
@@ -21,22 +21,14 @@ const itemSchema = new mongoose.Schema({
 // Model for defined schema itemSchema
 const Item = mongoose.model("Item", itemSchema);
 
-
-const date = require(__dirname + "/date.js");
-
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-
-const day = date.getDate();
-
   res.render("list", {listTitle: day, newListItems: items});
-
 });
 
 app.post("/", function(req, res){
