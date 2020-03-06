eba7e57e6cfb5e34da688301063d9fc4f191ec00
3rdBench <benjie.work@gmail.com>
2020-03-06 22:28:31 +0800

    Refactor root route's app.post() to allow adding new items into the 'Today' list.

--- app.js
+++ app.js
@@ -64,17 +64,21 @@ app.get("/", function(req, res) {
   });
 });
 
+// Add a new item to the 'Today' list
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  // Save list item submitted by the user
+  const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  // Corresponding document to save item list into the database
+  const item = new Item({
+    name: itemName
+  });
+
+  // Save item & redirects back to 'Today' list page
+  item.save();
+  res.redirect("/");
+
 });
 
 app.get("/work", function(req,res){
